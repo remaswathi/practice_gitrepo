2bde7ee27b1ef7ed9d2584d10a7b162b95e465f4
remaswathi <swathir.ece405@gmail.com>
2025-06-02 12:56:18 +0530

    practice new repo

---
+++ src/test/java/firstOne/Practice_FirstOne.java
@@ -0,0 +1,16 @@
+package firstOne;
+
+import org.testng.annotations.Test;
+
+public class Practice_FirstOne {
+ @Test
+ public void practiceGit() {
+	 System.out.println("copy new repo into gitcammand");
+	 
+ }
+ 
+ public void checkRepo() {
+	 System.out.println("check repository presence");
+	 
+ }
+}
